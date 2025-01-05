ef770bf4dfd6ea9e37a1e1de3b263ac944c43a44
SameeLaughs <108235389+SameeLaughs@users.noreply.github.com>
2025-01-05 16:47:55 -0500

    Create armavir.py

---
+++ armavir.py
@@ -0,0 +1,15 @@
+{
+    "lines": [
+        {
+            "lineNumber": 1,
+            "sentence": "Arma virumque cano",
+            "translation": "I sing of arms and the man",
+            "words": [
+                { "word": "Arma", "parsing": "Accusative, Plural", "meaning": "arms, weapons", "usage": "Serves as metonymy for tools" },
+                { "word": "virumque", "parsing": "Accusative, Singular", "meaning": "man", "usage": "Object" },
+                { "word": "cano", "parsing": "1st Person Singular, Present", "meaning": "sing", "usage": "Verb" }
+            ]
+        }
+ 
+     ]
+}
